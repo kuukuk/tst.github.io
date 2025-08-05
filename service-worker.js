f70b5b4ed006a3c93857b1a0523f1aa1ccc53432
kuukuk <kuukuk2@gmail.com>
2025-08-05 23:20:38 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+self.addEventListener('push', function(event) {
+  // In diesem Beispiel wird die Benachrichtigung direkt vom clientseitigen JavaScript ausgelöst.
+  // Wenn Sie Benachrichtigungen von einem Server senden möchten, würde der Code hier stehen.
+});

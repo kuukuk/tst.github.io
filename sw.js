59bbbe92f8cc061a8ad5b2ba65502d9abeed8eac
kuukuk <kuukuk2@gmail.com>
2025-08-05 23:12:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
-// Muss im gleichen Verzeichnis liegen
+// sw.js
+
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
+
+  // Optional: Wenn du willst, dass beim Klick eine Seite geöffnet wird
   event.waitUntil(
     clients.openWindow('/')
   );
